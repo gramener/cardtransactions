0c24749adb10905a68df721ca47a0577c667c2dd
S Anand <root.node@gmail.com>
2024-05-15 17:29:21 +0800

    ENH: Basic scaffolding

---
+++ script.js
@@ -0,0 +1,100 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import { render, html } from "https://cdn.jsdelivr.net/npm/lit-html@3/+esm";
+import { num, pc } from "https://cdn.jsdelivr.net/npm/@gramex/ui/dist/format.js";
+import {
+  insightTree,
+  INDEX,
+  GROUP,
+  LEVEL,
+  RANK,
+  IMPACT,
+} from "https://cdn.jsdelivr.net/npm/@gramex/insighttree@3.2/dist/insighttree.js";
+
+const $output = document.querySelector("#output");
+
+const $loading = html`
+  <div class="text-center my-5 fs-1">
+    <div class="spinner-border text-center" style="width: 6rem; height: 6rem" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+`;
+
+const config = await fetch("config.json").then((r) => r.json());
+const metrics = config.metrics;
+
+$output.replaceChildren();
+
+render(
+  Object.entries(config.groups).map(
+    ([group, { title, info }]) => html`
+      <h2 class="display-6 text-center mt-5 mb-3">${title}</h2>
+      <p class="info">${info}</p>
+      <div id="tree-${group}">${$loading}</div>
+    `,
+  ),
+  $output,
+);
+
+// Fetch all groups' data in parallel
+Object.entries(config.groups).forEach(async ([group, { groups, leaf, start, showKey }]) => {
+  const data = await fetch(`${group}.json`).then((r) => r.json());
+  const $tree = document.querySelector(`#tree-${group}`);
+  $tree.replaceChildren();
+  const color = d3.scaleSequential(d3.interpolateRdYlGn).domain([0.04, 0.03]);
+  const tree = insightTree(`#tree-${group}`, {
+    data: data,
+    groups,
+    metrics,
+    sort: "-Is Fraud?",
+    impact: (row) => (row["Is Fraud?"] / row["Count"]) * Math.log(row["Count"]) ** 1.5,
+    render: (el, { tree }) =>
+      render(
+        html` <div><input id="level-${group}" type="range" min="1" max="30" value="${start}" class="form-range" /></div>
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Group</th>
+                  <th class="text-end">Fraud</th>
+                  <th class="text-end">Transactions</th>
+                  <th class="text-end">Fraud rate</th>
+                  <th class="text-end">Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${tree.map(
+                  (row) => html`
+                  <tr data-insight-level="${row[LEVEL]}" data-insight-rank="${row[RANK]}">
+                    <td>#${row[RANK]}</th>
+                    <td style="padding-left:${row[LEVEL] * 1.5}rem">
+                      <span class="insight-toggle"></span>
+                      ${row[LEVEL] && showKey ? `${groups[row[LEVEL] - 1]} = ` : "" } ${row[GROUP]}
+                    </td>
+                    <td class="text-end">${num(row["Is Fraud?"])}</td>
+                    <td class="text-end">${num(row["Count"])}</td>
+                    <td class="text-end contrast-color" style="background-color: ${color(row["Is Fraud?"] / row["Count"])}">${pc(row["Is Fraud?"] / row["Count"])}</td>
+                    <td class="text-end">$${num(row["Amount"])}</td>
+                  </tr>`,
+                )}
+              </tbody>
+            </table>
+          </div>`,
+        $tree,
+      ),
+  });
+  const $level = document.querySelector(`#level-${group}`);
+  const update = leaf ? (rank) => updateLeaf(tree, rank) : (rank) => tree.update({ rank });
+
+  update($level.value);
+  $level.addEventListener("input", (e) => update(e.target.value));
+});
+
+function updateLeaf(insightTree, rank) {
+  rank = insightTree.tree[insightTree.leaves[rank - 1]]?.[RANK] ?? insightTree.tree.length;
+  return insightTree
+    .show((row) => row[RANK] <= rank && insightTree.leaves.includes(row[INDEX]))
+    .classed("insight-highlight", (row) => row[RANK] <= rank)
+    .classed("insight-current", (row) => row[RANK] == rank);
+}
---
+++ setup.py
@@ -0,0 +1,36 @@
+import json
+import os
+import yaml
+import pandas as pd
+
+folder = os.path.dirname(__file__)
+
+
+def main():
+    source = os.path.join(folder, "transactions.parquet")
+    config_file = os.path.join(folder, "config.yaml")
+    with open(config_file) as handle:
+        config = yaml.safe_load(handle)
+    with open(os.path.join(folder, "config.json"), "w") as handle:
+        json.dump(config, handle)
+    source_mtime = max(os.path.getmtime(src) for src in [source, config_file, __file__])
+
+    for group, conf in config["groups"].items():
+        target = os.path.join(folder, f"{group}.json")
+        if os.path.exists(target) and source_mtime < os.path.getmtime(target):
+            continue
+        df = pd.read_parquet(source)
+        grouped = df.groupby(conf["groups"]).agg(
+            {
+                "Amount": "sum",
+                "Is Disputed?": "sum",
+                "Is Fraud?": "sum",
+                "Dispute Type": "count",
+            }
+        )
+        grouped.rename(columns={"Dispute Type": "Count"}, inplace=True)
+        grouped.reset_index().to_json(target, orient="records", double_precision=2)
+
+
+if __name__ == "__main__":
+    main()
